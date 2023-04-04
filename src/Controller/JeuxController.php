58abbc5193452631a978355227cc62631734a66a
ahmedchouchene99 <achouchene03@gmail.com>
2023-04-04 05:20:57 +0100

    Test

---
+++ src/Controller/JeuxController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Jeux;
+use App\Form\JeuxType;
+use App\Repository\JeuxRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+
+#[Route('/jeux')]
+class JeuxController extends AbstractController
+{
+    #[Route('/', name: 'app_jeux_index', methods: ['GET'])]
+    public function index(JeuxRepository $jeuxRepository): Response
+    {
+        return $this->render('jeux/index.html.twig', [
+            'jeuxes' => $jeuxRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_jeux_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, SluggerInterface $slugger , JeuxRepository $jeuxRepository): Response
+    {
+        $jeux = new Jeux();
+        $form = $this->createForm(JeuxType::class, $jeux);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $brochureFile = $form->get('image')->getData();
+
+            if ($brochureFile) {
+                $originalFilename = pathinfo($brochureFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$brochureFile->guessExtension();
+
+                try {
+                    $brochureFile->move(
+                        $this->getParameter('brochures_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                }
+
+                $jeux->setImage($newFilename);
+            }
+
+            $jeuxRepository->save($jeux, true);
+
+            return $this->redirectToRoute('app_jeux_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('jeux/new.html.twig', [
+            'jeux' => $jeux,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_jeux_show', methods: ['GET'])]
+    public function show(Jeux $jeux): Response
+    {
+        return $this->render('jeux/show.html.twig', [
+            'jeux' => $jeux,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_jeux_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Jeux $jeux, SluggerInterface $slugger , JeuxRepository $jeuxRepository): Response
+    {
+        $form = $this->createForm(JeuxType::class, $jeux);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $brochureFile = $form->get('image')->getData();
+
+            if ($brochureFile) {
+                $originalFilename = pathinfo($brochureFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$brochureFile->guessExtension();
+    
+                try {
+                    $brochureFile->move(
+                        $this->getParameter('brochures_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                }
+    
+                $jeux->setImage($newFilename);
+            }
+    
+            $jeuxRepository->save($jeux, true);
+
+            return $this->redirectToRoute('app_jeux_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('jeux/edit.html.twig', [
+            'jeux' => $jeux,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_jeux_delete', methods: ['POST'])]
+    public function delete(Request $request, Jeux $jeux, JeuxRepository $jeuxRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$jeux->getId(), $request->request->get('_token'))) {
+            $jeuxRepository->remove($jeux, true);
+        }
+
+        return $this->redirectToRoute('app_jeux_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Entity/Jeux.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\JeuxRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: JeuxRepository::class)]
+class Jeux
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message:"title is required.")]
+    #[Assert\Length(
+        min: 3,
+        max: 10,
+        minMessage: 'Your title must be at least {{ limit }} characters long',
+        maxMessage: 'Your  title cannot be longer than {{ limit }} characters',
+    )]
+    private ?string $titre = null;
+
+    #[ORM\Column(length: 255)]
+   
+    private ?string $image = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    #[Assert\GreaterThanOrEqual("today", message:"The date must be equal or greater than today.")]
+    private ?\DateTimeInterface $date_debut = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    #[Assert\Expression(
+        "this.getDateFin() >= this.getDateDebut()",
+        message: "The end date must be greater than or equal to the start date"
+    )]
+    private ?\DateTimeInterface $date_fin = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message:"produit is required.")]
+    #[Assert\Length(
+        min: 3,
+        max: 10,
+        minMessage: 'Your product must be at least {{ limit }} characters long',
+        maxMessage: 'Your  product cannot be longer than {{ limit }} characters',
+    )]
+    private ?string $produit = null;
+
+    #[ORM\Column]
+    #[Assert\NotBlank(message:"price is required.")]
+    #[Assert\GreaterThan(value:0, message:"Le prix doit etre supérieur à zéro.")]
+    private ?float $prix = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getDateDebut(): ?\DateTimeInterface
+    {
+        return $this->date_debut;
+    }
+
+    public function setDateDebut(\DateTimeInterface $date_debut): self
+    {
+        $this->date_debut = $date_debut;
+
+        return $this;
+    }
+
+    public function getDateFin(): ?\DateTimeInterface
+    {
+        return $this->date_fin;
+    }
+
+    public function setDateFin(\DateTimeInterface $date_fin): self
+    {
+        $this->date_fin = $date_fin;
+
+        return $this;
+    }
+
+    public function getProduit(): ?string
+    {
+        return $this->produit;
+    }
+
+    public function setProduit(string $produit): self
+    {
+        $this->produit = $produit;
+
+        return $this;
+    }
+
+    public function getPrix(): ?float
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(float $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+}
